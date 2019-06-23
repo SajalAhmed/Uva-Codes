35d2890bcb128d99ff5c77b181f5cc324a4c5958
Sajal Ahmed <sajal6292@gmail.com>
2019-06-24 00:17:05 +0600

    UVA-11475

--- UVA_11475 - Extend to Palindrome/solve.cpp
+++ UVA_11475 - Extend to Palindrome/solve.cpp
@@ -13,32 +13,28 @@ void kmpPreprocess(int m){
 		b[i+1] = j;
 	} 
 } 
-int kmpSearch(int n, int m){
+int kmpSearch(int n){
 	int j = 0;
 	for(int i = 0; i < n; i++){
 		while (j >= 0 && T[i] != P[j]){
 			j = b[j];
 		}
 		j++; 
-		if(m == j) break;
 	}
 	return j;
 }
 
 int main(){
 //freopen("input.txt", "r", stdin);
- 	int t, m, n, temp;
- 	scanf("%d", &t);
- 	while(t--){
- 	    scanf("%*d%d", &m);
- 	    cin>>T;
- 	    n = T.size();
- 	    for(int i =  1; i < m; i++){
- 	       cin>>P;
- 	       kmpPreprocess(n);
- 	       temp = kmpSearch(T.size(), n);
- 	       T += P.substr(temp, n);
- 	    }
- 	    cout<<T.size()<<endl;
+ 	int t, v, n;
+ 	while(cin>> T){
+ 	  P = T;
+ 	  n = P.size();
+ 	  reverse(P.begin(), P.end());
+ 	  kmpPreprocess(n);	
+ 	  for(int i = kmpSearch(n);i < n; i++){
+ 	   	T += P[i];
+ 	  }
+ 	  cout<<T<<endl;
  	}
 }
