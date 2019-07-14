fc81d1bdfec3c58f4343409f6b2f978facbbe224
Sajal Ahmed <sajal6292@gmail.com>
2019-07-14 20:51:11 +0600

    first-commit

--- UVA_575 - Skew Binary/solve.cpp
+++ UVA_575 - Skew Binary/solve.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 int main(){
 	string str;
-	int sz , temp, sum = 0;
+	int size_ , temp, sum = 0;
 	while(cin>>str){
-	 	sz = str.size();
-	 	if(sz == 1 && str[0] == '0') break;
-	 	for(int i = 0 ; i < sz; i++){
-	 		temp = pow(2, sz - i);
+	 	size_ = str.size();
+	 	if(size_ == 1 && str[0] == '0') break;
+	 	for(int i = 0 ; i < size_; i++){
+	 		temp = pow(2, size_ - i);
 	 	 	sum += (str[i] - '0') * (temp - 1);
 	 	}
 	 	printf("%d\n", sum);
